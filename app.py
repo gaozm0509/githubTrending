c06e8b52532650c8217543e220d8d0c2994d9063
Apple <apple@AppledeMacBook-Pro.local>
2019-07-31 19:34:36 +0800

    修改readme

--- app.py
+++ app.py
@@ -59,5 +59,5 @@ class Trending(flask_restful.Resource):
 
 
 api.add_resource(Trending, '/')
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+    # app.run(host='lo', port=5000)
